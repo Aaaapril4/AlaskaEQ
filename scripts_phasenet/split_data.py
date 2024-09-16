05537dc5b6dc6d6f617254585ac641b5ab04e2e2
Giaaaaaa <gia.jyq@gmail.com>
2024-09-16 15:38:43 -0400

    modify the merge

--- scripts_phasenet/split_data.py
+++ scripts_phasenet/split_data.py
@@ -9,10 +9,10 @@ comm = MPI.COMM_WORLD  # pylint: disable=c-extension-no-member
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-basedir = '/mnt/scratch/jieyaqi/alaska/final/pntf_alaska_v1'
+basedir = '/mnt/scratch/jieyaqi/alaska/final/pntf_alaska_all_iter2'
 root = Path(basedir)
 datad = root / "data"
-nperdir = 155
+nperdir = 300
 
 
 def get_process_list_this_rank(process_list: list):
@@ -46,8 +46,8 @@ def process_dir_rank(dirnum_this_rank: list, dirmap: dict):
     '''
     for num in dirnum_this_rank:
         generate_csv(str(dirmap[num]), '/mnt/home/jieyaqi/code/AlaskaEQ/data/station.txt', str(root / f'statime{num}.csv'))
-        generate_yaml('scripts_phasenet/alaska_v2.yaml', str(Path('/mnt/home/jieyaqi/code/PhaseNet-TF/configs/experiment') / f'alaska_v2{num}.yaml'), num)
-        per_index(num, str(dirmap[num]), str(root / f'data{num}.sqlite'))
+        generate_yaml('scripts_phasenet/alaska.yaml', str(Path('/mnt/home/jieyaqi/code/PhaseNet-TF/configs/experiment') / f'alaska{num}.yaml'), num)
+        per_index(str(dirmap[num]), str(root / f'data{num}.sqlite'))
     return
 
 
